ed16d7bb86b4939040a4c94f135a82504445776c
Niccolò Prada <14167048+NPrada@users.noreply.github.com>
2025-01-07 15:56:39 +0000

    basica line bot to pick a date time

--- index.ts
+++ index.ts
@@ -7,18 +7,18 @@ import {
   MiddlewareConfig,
   webhook,
   HTTPFetchError,
-} from '@line/bot-sdk';
-import express, {Application, Request, Response} from 'express';
-import 'dotenv/config'
+} from "@line/bot-sdk";
+import express, { Application, Request, Response } from "express";
+import "dotenv/config";
 
 
 // Setup all LINE client and Express configurations.
 const clientConfig: ClientConfig = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || '',
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || "",
 };
 
 const middlewareConfig: MiddlewareConfig = {
-  channelSecret: process.env.CHANNEL_SECRET || '',
+  channelSecret: process.env.CHANNEL_SECRET || "",
 };
 
 const PORT = process.env.PORT || 3000;
@@ -29,16 +29,16 @@ const client = new messagingApi.MessagingApiClient(clientConfig);
 // Create a new Express application.
 const app: Application = express();
 
-
 // Function handler to receive the text.
-const textEventHandler = async (event: webhook.Event): Promise<MessageAPIResponseBase | undefined> => {
+const textEventHandler = async (
+  event: webhook.Event,
+): Promise<MessageAPIResponseBase | undefined> => {
   // Process all variables here.
-  console.log('new event', event)
+  console.log("new event", event);
+  const userId = event.source.userId;
 
-  
-  
   // Check if for a text message
-  if (event.type !== 'message' || event.message.type !== 'text') {
+  if (event.type !== "message" || event.message.type !== "text") {
     return;
   }
 
@@ -47,17 +47,32 @@ const textEventHandler = async (event: webhook.Event): Promise<MessageAPIRespons
   // Check if message is repliable
   if (!event.replyToken) return;
 
-  // Create a new message.
-  // Reply to the user.
+
+  const earliestDateString = formatDateToDateString(addHoursToDate(new Date(), 3))
   await client.replyMessage({
-    replyToken:event.replyToken,
-    messages: [{
-      type: 'text',
-      text: event.message.text,
-    }],
+    replyToken: event.replyToken,
+    messages: [
+      {
+        type: "template",
+        altText: "日付を選択してください",
+        template: {
+          type: "buttons",
+          text: "日付を選択してください",
+          actions: [
+            {
+              type: "datetimepicker",
+              label: "Select date",
+              data: `action=selectDate&userId=${userId}`,
+              mode: "datetime",
+              initial: earliestDateString,
+              max: formatDateToDateString(addMonthsToDate(new Date(), 6)),
+              min: earliestDateString,
+            },
+          ],
+        },
+      },
+    ],
   });
-
-
 };
 
 // Register the LINE middleware.
@@ -66,19 +81,16 @@ const textEventHandler = async (event: webhook.Event): Promise<MessageAPIRespons
 
 // Route handler to receive webhook events.
 // This route is used to receive connection tests.
-app.get(
-  '/',
-  async (_: Request, res: Response): Promise<Response> => {
-    return res.status(200).json({
-      status: 'success',
-      message: 'Connected successfully!',
-    });
-  }
-);
+app.get("/", async (_: Request, res: Response): Promise<Response> => {
+  return res.status(200).json({
+    status: "success",
+    message: "Connected successfully!",
+  });
+});
 
 // This route is used for the Webhook.
 app.post(
-  '/callback',
+  "/callback",
   middleware(middlewareConfig),
   async (req: Request, res: Response): Promise<Response> => {
     const callbackRequest: webhook.CallbackRequest = req.body;
@@ -92,7 +104,7 @@ app.post(
         } catch (err: unknown) {
           if (err instanceof HTTPFetchError) {
             console.error(err.status);
-            console.error(err.headers.get('x-line-request-id'));
+            console.error(err.headers.get("x-line-request-id"));
             console.error(err.body);
           } else if (err instanceof Error) {
             console.error(err);
@@ -100,21 +112,47 @@ app.post(
 
           // Return an error message.
           return res.status(500).json({
-            status: 'error',
+            status: "error",
           });
         }
-      })
+      }),
     );
 
     // Return a successful message.
     return res.status(200).json({
-      status: 'success',
+      status: "success",
       results,
     });
-  }
+  },
 );
 
 // Create a server and listen to it.
 app.listen(PORT, () => {
   console.log(`Application is live and listening on port ${PORT}`);
 });
+
+
+
+function addMonthsToDate(date: Date, months: number) {
+  const newDate = new Date(date);
+  newDate.setMonth(newDate.getMonth() + months);
+  return newDate;
+}
+
+function addHoursToDate(date: Date, hours: number) {
+  const newDate = new Date(date);
+  newDate.setHours(newDate.getHours() + hours);
+  return newDate;
+}
+
+function formatDateToDateString(date: Date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+
+  const dateString = `${year}-${month}-${day}t${hours}:${minutes}`;
+
+  return dateString;
+}
