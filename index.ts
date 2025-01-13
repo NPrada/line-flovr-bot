1abf3e6c8d7d0a339ba0ffd923d4dda7ab265817
Niccolò Prada <14167048+NPrada@users.noreply.github.com>
2025-01-13 07:47:01 +0000

    partial working flow

--- index.ts
+++ index.ts
@@ -12,7 +12,8 @@ import express, { Application, Request, Response } from "express";
 import "dotenv/config";
 import { Client } from "@notionhq/client";
 
-const notionOrdersDatabaseId = "174ccfeeb33081fc9ffcc2fffa2dda4c";
+const notionOrdersDatabaseId = "f131d30fddd24b1faefd80fc7b430375";
+const shopPhoneNumber = "055-993-1187";
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
@@ -50,7 +51,7 @@ async function updateDbRowByUserId(
   newVal: string,
 ) {
   // 1. Find the page via a filter on the 'UserId' title field.
-  const userIdToFind = userId;
+  // const userIdToFind = userId;
   // const queryRes = await notion.databases.query({
   //   database_id: notionOrdersDatabaseId,
   //   filter: {
@@ -65,15 +66,15 @@ async function updateDbRowByUserId(
     filter: {
       and: [
         {
-          property: 'UserId', // "userId" column is your title property
+          property: "UserId", // "userId" column is your title property
           title: {
             equals: userId,
           },
         },
         {
-          property: 'Status', // "Status" is a status property
+          property: "Status", // "Status" is a status property
           status: {
-            equals: 'Form not complete',
+            equals: "Form not complete",
           },
         },
       ],
@@ -108,18 +109,34 @@ async function updateDbRowByUserId(
   console.log("Updated page:", updatedPage);
 }
 
+function convertQueryString(queryString: string) {
+  // eg input:  queryString = 'action=selectDate&userId=U77ba2a8cdb3cd481ddf80ade6079877b';
+
+  // Use URLSearchParams to parse the string
+  const params = new URLSearchParams(queryString);
+
+  // Convert to an object
+  let keyValues: Record<string, string> = {};
+  for (const [key, value] of params.entries()) {
+    keyValues[key] = value;
+  }
+  return keyValues;
+}
+
 (async () => {
   // const res = await notion.databases.update({database_id: notionOrdersDatabaseId, properties: {
   //   // UserId: '123'
-
   // }})
-
   // const res = await createNewDbRow("1234", new Date());
-  const res = await updateDbRowByUserId("1234", "User Name", "New Name")
 
+  console.log("hello");
+  const res = await updateDbRowByUserId(
+    "U77ba2a8cdb3cd481ddf80ade6079877b",
+    "Purpose",
+    "Item",
+  );
   // const res = await notion.databases.retrieve({database_id: notionOrdersDatabaseId})
-
-  console.log("res", res);
+  // console.log("res", res);
 })();
 
 // Setup all LINE client and Express configurations.
@@ -147,43 +164,177 @@ const textEventHandler = async (
   console.log("new event", event);
   const userId = event.source.userId;
 
+  if (event.type === "postback") {
+    const postbackData = event.postback;
+
+    //SELECTED DATE RESPONSE
+    if (postbackData.data.includes("action=selectDate")) {
+      const data = convertQueryString(postbackData.data);
+      const selectedDate = postbackData.params.datetime;
+      const userId = data.userId;
+
+      const res = await createNewDbRow(userId, new Date(selectedDate));
+
+      if (!event.replyToken) return;
+
+      //SELECT Purpose
+      await client.replyMessage({
+        replyToken: event.replyToken,
+        messages: [
+          {
+            type: "text",
+            text: `${selectedDate}`,
+          },
+
+          {
+            type: "template",
+            altText: "Three-button menu",
+            template: {
+              type: "buttons",
+              title: "用途を選んでください",
+              text: "以下から選んでください。",
+              actions: [
+                {
+                  type: "postback",
+                  label: "誕生日",
+                  text: "誕生日",
+                  data: `action=purposeSelect&itemType=誕生日-birthday&userId=${userId}`,
+                },
+                {
+                  type: "postback",
+                  label: "お祝",
+                  text: "お祝",
+                  data: `action=purposeSelect&itemType=お祝-celebration&userId=${userId}`,
+                },
+                {
+                  type: "postback",
+                  label: "お供え",
+                  text: "お供え",
+                  data: `action=purposeSelect&itemType=お供え-offering&userId=${userId}`,
+                },
+                {
+                  type: "postback",
+                  label: "ご自宅用",
+                  text: "ご自宅用",
+                  data: `action=purposeSelect&itemType=ご自宅用-homeuse&userId=${userId}`,
+                },
+              ],
+            },
+          },
+        ],
+      });
+    } else if (postbackData.data.includes("action=purposeSelect")) {
+      const data = convertQueryString(postbackData.data);
+      console.log(data);
+      const itemType = data.itemType;
+      const userId = data.userId;
+      console.log(userId, "Purpose", itemType);
+      const res = await updateDbRowByUserId(userId, "Purpose", itemType);
+      console.log("updated");
+      if (!event.replyToken) return;
+
+      //SELECT COLOR
+      await client.replyMessage({
+        replyToken: event.replyToken,
+        messages: [
+          {
+            type: "template",
+            altText: "Three-button menu",
+            template: {
+              type: "buttons",
+              title: "色味を選んでください",
+              text: "以下から選んでください。",
+              actions: [
+                {
+                  type: "postback",
+                  label: "赤系",
+                  text: "赤系",
+                  data: `action=colorSelect&itemType=赤系-red&userId=${userId}`,
+                },
+                {
+                  type: "postback",
+                  label: "ピンク系",
+                  text: "ピンク系",
+                  data: `action=colorSelect&itemType=ピンク系-pink&userId=${userId}`,
+                },
+                {
+                  type: "postback",
+                  label: "黄色・オレンジ系",
+                  text: "黄色・オレンジ系",
+                  data: `action=colorSelect&itemType=黄色・オレンジ系-yellow-orange&userId=${userId}`,
+                },
+                {
+                  type: "postback",
+                  label: "ミックス",
+                  text: "ミックス",
+                  data: `action=colorSelect&itemType=ミックス-mix&userId=${userId}`,
+                },
+              ],
+            },
+          },
+        ],
+      });
+    } else if (postbackData.data.includes("action=colorSelect")) {
+      const data = convertQueryString(postbackData.data);
+      const itemType = data.itemType;
+      const userId = data.userId;
+
+      const res = await updateDbRowByUserId(userId, "Color", itemType);
+
+      if (!event.replyToken) return;
+
+      //SELECT BUDGET
+      await client.replyMessage({
+        replyToken: event.replyToken,
+        messages: [
+          {
+            type: "text",
+            text: "ご予算を入力してください。（税込み）",
+          },
+        ],
+      });
+    }
+  }
+
   // Check if for a text message
   if (event.type !== "message" || event.message.type !== "text") {
     return;
   }
 
   // Process all message related variables here.
+  if (event.message.text.toLowerCase().trim() == "hello") {
+    // Check if message is repliable
+    if (!event.replyToken) return;
 
-  // Check if message is repliable
-  if (!event.replyToken) return;
+    const earliestDateString = formatDateToDateString(
+      addHoursToDate(new Date(), 3),
+    );
 
-  const earliestDateString = formatDateToDateString(
-    addHoursToDate(new Date(), 3),
-  );
-  await client.replyMessage({
-    replyToken: event.replyToken,
-    messages: [
-      {
-        type: "template",
-        altText: "日付を選択してください",
-        template: {
-          type: "buttons",
-          text: "日付を選択してください",
-          actions: [
-            {
-              type: "datetimepicker",
-              label: "Select date",
-              data: `action=selectDate&userId=${userId}`,
-              mode: "datetime",
-              initial: earliestDateString,
-              max: formatDateToDateString(addMonthsToDate(new Date(), 6)),
-              min: earliestDateString,
-            },
-          ],
+    await client.replyMessage({
+      replyToken: event.replyToken,
+      messages: [
+        {
+          type: "template",
+          altText: `3時間以内のご注文の場合は、${shopPhoneNumber}までお電話ください。それ以外の方は以下より日時を選んでください。`,
+          template: {
+            type: "buttons",
+            text: `3時間以内のご注文の場合は、${shopPhoneNumber}までお電話ください。それ以外の方は以下より日時を選んでください。`,
+            actions: [
+              {
+                type: "datetimepicker",
+                label: "Select date",
+                data: `action=selectDate&userId=${userId}`,
+                mode: "datetime",
+                initial: earliestDateString,
+                max: formatDateToDateString(addMonthsToDate(new Date(), 6)),
+                min: earliestDateString,
+              },
+            ],
+          },
         },
-      },
-    ],
-  });
+      ],
+    });
+  }
 };
 
 // Register the LINE middleware.
